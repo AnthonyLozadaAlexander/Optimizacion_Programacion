7bb32d05ca787698ee8157e930675b262f9b263c
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-09 03:21:36 -0500

    Agrega script para calcular y mostrar puntos críticos de una función
    Se implementa un nuevo script en PuntoCritico.py que utiliza la biblioteca sympy para calcular la derivada de una función cuadrática y encontrar sus puntos críticos. El script imprime la derivada y los puntos críticos encontrados, facilitando el análisis de la función.

---
+++ Concepto/PuntoCritico.py
@@ -0,0 +1,16 @@
+﻿import sympy as sp
+
+x = sp.Symbol("x") # variable x
+
+f = (-x ** 2 + 10*x - 15) # definicion de funcion
+
+derivada = sp.diff(f, x) # se calcula la derivada
+print(f"La derivada es: {derivada}")
+
+puntosCriticos = sp.solve(derivada, x) # se almacenan los puntos criticos
+
+# Itera el arreglo de puntosCriticos
+for i in range(len(puntosCriticos)):
+	puntosCrit = puntosCriticos[i]
+	print(f"Punto Critico[{i+1}]: {puntosCrit}")
+
